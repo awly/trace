070d35134988cb93cecb4ca297ab22b77424802f
Andrew Lytvynov <lytvynov.a.v@gmail.com>
2014-10-27 13:53:54 -0700

    initial commit

---
+++ trace.go
@@ -0,0 +1,92 @@
+// package trace provides convenient functionality to trace HTTP requests.
+// Main functionality consists of prepending unique token to all logs related to request.
+//
+// Basic usage: wrap your top-level http.Handler with trace.Handler and use trace.Log* functions
+// instead of log.Print*.
+// Use trace.Token to retrieve unique token for request (for example to write it in response body/header).
+//
+// This library was created to help debugging services that handle multiple concurrent requests and
+// be able to extract only relevant logs for it.
+package trace
+
+import (
+	"crypto/md5"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/context"
+)
+
+const requestTokenKey = "_token"
+
+// Handler wraps h, generating new token for it.
+// It also logs request beginning and ending.
+// gorilla/context.Clear is called after handler is done.
+func Handler(h http.Handler) http.Handler {
+	return context.ClearHandler(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		token := fmt.Sprintf("%x", md5.Sum([]byte(r.URL.String()+r.RemoteAddr+time.Now().String())))
+		context.Set(r, requestTokenKey, token)
+		Logln(r, "new request", r.Method, r.URL)
+		h.ServeHTTP(rw, r)
+		Logln(r, "done")
+	}))
+}
+
+// NoLogHandler is like Handler but it doesn't do any logging.
+func NoLogHandler(h http.Handler) http.Handler {
+	return context.ClearHandler(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		token := fmt.Sprintf("%x", md5.Sum([]byte(r.URL.String()+r.RemoteAddr+time.Now().String())))
+		context.Set(r, requestTokenKey, token)
+		h.ServeHTTP(rw, r)
+	}))
+}
+
+// NoClearHandler is like Handler but it doesn't clear gorilla/context.
+func NoClearHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		token := fmt.Sprintf("%x", md5.Sum([]byte(r.URL.String()+r.RemoteAddr+time.Now().String())))
+		context.Set(r, requestTokenKey, token)
+		Logln(r, "new request", r.Method, r.URL)
+		h.ServeHTTP(rw, r)
+		Logln(r, "done")
+	})
+}
+
+// NoLogClearHandler is like Handler but it doesn't do any logging and doesn't clear gorilla/context.
+func NoLogClearHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		token := fmt.Sprintf("%x", md5.Sum([]byte(r.URL.String()+r.RemoteAddr+time.Now().String())))
+		context.Set(r, requestTokenKey, token)
+		h.ServeHTTP(rw, r)
+	})
+}
+
+// Token returns generated token for request or empty string it's not present
+func Token(r *http.Request) string {
+	tok := context.Get(r, requestTokenKey)
+	if toks, ok := tok.(string); ok {
+		return toks
+	}
+	return ""
+}
+
+// Log forwards vals to log.Print and prepends request token
+func Log(r *http.Request, vals ...interface{}) {
+	tok := Token(r)
+	log.Print(append([]interface{}{tok}, vals...)...)
+}
+
+// Logln forwards vals to log.Println and prepends request token
+func Logln(r *http.Request, vals ...interface{}) {
+	tok := Token(r)
+	log.Println(append([]interface{}{tok}, vals...)...)
+}
+
+// Logf forwards f and vals to log.Printf and prepends request token
+func Logf(r *http.Request, f string, vals ...interface{}) {
+	tok := Token(r)
+	f = "%s " + f
+	log.Printf(f, append([]interface{}{tok}, vals...)...)
+}
